03b80dcadaaffadcf36679fbd2e2c9cf4a910e9a
tangyulong <tangyulong@aituyou.com>
2015-05-06 18:40:18 +0800

    1. adapter to rabbitmq-c v0.60.

--- examples/example_get.cpp
+++ examples/example_get.cpp
@@ -1,38 +1,42 @@
+#include <unistd.h>
 #include "AMQPcpp.h"
 
-int main () {
+int main (int argc, char** argv) {
+    try {
+        AMQP amqp("123456:admin@127.0.0.1:5672//");
 
+        string exchange_name("amq.topic");
+        string exchange_type("topic");
+        AMQPExchange* exchange = amqp.createExchange(exchange_name);
+        exchange->Declare(exchange_name, exchange_type, AMQP_DURABLE);
 
+        string queue_name("test");
+        AMQPQueue * qu2 = amqp.createQueue(queue_name);
+        qu2->Declare();
+        qu2->Bind(exchange_name, queue_name);
 
-	try {
-//		AMQP amqp("123123:akalend@localhost/private");
-		AMQP amqp("123123:akalend@localhost:5673/private");		
+        int i = 0;
+        while (1) {
+            qu2->Get(AMQP_NOACK);
+            AMQPMessage * m= qu2->getMessage();
+            cout << ++i << "th, "<< "get message count: " <<  m->getMessageCount() + 1 << "." << endl;
+            if (m->getMessageCount() > -1) {
+                uint32_t j = 0;
+                cout << "message\n";
+                cout << "content:"<< m->getMessage(&j) << endl;
+                cout << "key: "<< m->getRoutingKey() << endl;
+                cout << "exchange: "<< m->getExchange() << endl;
+                cout << "Content-type: "<< m->getHeader("Content-type") << endl;
+                cout << "Content-encoding: "<< m->getHeader("Content-encoding") << endl;
+                cout << "\n";
+            }
+            else {
+                sleep(60);
+            }
+        }
+    } catch (AMQPException e) {
+        std::cout << e.getMessage() << std::endl;
+    }
 
-		AMQPQueue * qu2 = amqp.createQueue("q2");
-		qu2->Declare();		
-		
-		
-		while (  1 ) {
-			qu2->Get(AMQP_NOACK);
-
-			AMQPMessage * m= qu2->getMessage();
-			
-			cout << "count: "<<  m->getMessageCount() << endl;											 
-			if (m->getMessageCount() > -1) {
-			uint32_t j = 0;
-			cout << "message\n"<< m->getMessage(&j) << "\nmessage key: "<<  m->getRoutingKey() << endl;
-			cout << "exchange: "<<  m->getExchange() << endl;											
-			cout << "Content-type: "<< m->getHeader("Content-type") << endl;	
-			cout << "Content-encoding: "<< m->getHeader("Content-encoding") << endl;	
-			} else 
-				break;				
-						
-							
-		}	
-	} catch (AMQPException e) {
-		std::cout << e.getMessage() << std::endl;
-	}
-
-	return 0;					
-
+    return 0;
 }
